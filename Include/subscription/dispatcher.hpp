111fc2f51c67cd83c733beb2cc54497d82a4d5c4
Schwartz <LSchwar9@its.jnj.com>
2025-07-02 16:31:32 +0300

    disp imp2

--- Include/subscription/dispatcher.hpp
+++ Include/subscription/dispatcher.hpp
@@ -1,34 +1,155 @@
 #pragma once
+// Schwartz Liran 
+
+
 #include <iostream>
 #include <mutex>
 #include <string>
 #include <vector>
+#include <queue>
+#include <thread>
+#include <chrono>
+
 
 
 
 namespace afu
 {
-	
-	class dispatcher_interface :
-		std::enable_shared_from_this<dispatcher_interface>
+
+	class async_action_context :
+		std::enable_shared_from_this<async_action_context>
 	{
-	public: 
+	public:
+		async_action_context() = default;
 
-		~dispatcher_interface() = default;
+		~async_action_context() = default;
 
-		virtual bool invoke() = 0;
-		
-		virtual bool begin_invoke() = 0;		
+		void begin_invoke()
+		{
+			run_action();
+		}
+
+	protected:
+
+		virtual void run_action() = 0;
+
 	};
 
 
-	class dispatcher_base : public dispatcher_interface
+	class dispatcher : public std::enable_shared_from_this<dispatcher>
 	{
 	public:
 
+		virtual void add_action(const async_action_context& _action)
+		{
+			std::lock_guard<std::mutex> lock(m_lock_invoke_thread);
+			std::shared_ptr<async_action_context> ac = std::make_shared<async_action_context>(_action);
+			m_action_q.push(ac);
+		}
+
+		virtual void add_list_action(const std::vector<std::shared_ptr<async_action_context>>& _action)
+		{
+			std::lock_guard<std::mutex> lock(m_lock_invoke_thread);
+			for(const auto& ac : _action)
+				m_action_q.push(ac);
+			
+		}
+
+		
+		~dispatcher()
+		{
+			m_still_running = false;
+			if (m_invoke_thread.joinable())
+			{
+				m_invoke_thread.join();
+			}
+		}
+
+		void begin_invoke()
+		{
+			if (m_invoke_thread.joinable() && 
+				m_still_running == true &&
+				m_action_q.empty() == false)
+			{
+				m_invoke_thread_cv.notify_all();
+			}
+		}
+
+		void begin_invoke(const async_action_context& _action)
+		{
+			add_action(_action);
+			if (m_invoke_thread.joinable() && 
+				m_still_running == true && 
+				m_action_q.empty() == false)
+			{
+				m_invoke_thread_cv.notify_all();
+			}
+		}
+
+		virtual void start()
+		{
+			try
+			{
+				m_invoke_thread = std::thread([&]()
+					{
+						m_id = std::this_thread::get_id();
+						m_still_running = true;
+						while (m_still_running)
+						{
+							std::unique_lock<std::mutex> lock(m_lock_invoke_thread);
+							m_invoke_thread_cv.wait_for(lock, std::chrono::seconds(5), [&]()
+								{
+									return (m_action_q.empty() == false);
+								}
+							);
+
+							while (!m_action_q.empty())
+							{
+								auto& action = m_action_q.front();
+								invoke(action);
+								m_action_q.pop();
+							}
+						}
+					});
+			}
+			catch (const std::exception&)
+			{
+
+			}
+		}
+
+		std::thread::id get_id()
+		{
+			if (m_invoke_thread.joinable())
+				return m_id;
+
+			return std::thread::id();
+		}
+
+	private:
 
 
+		bool invoke(const std::shared_ptr<afu::async_action_context>& _action)
+		{
+			try
+			{
+				_action->begin_invoke();
+			}
+			catch (const std::exception&)
+			{
+				return false;
+			}
+			
+			return true;
+		}
 
+		std::thread m_invoke_thread;
+		std::mutex m_lock_invoke_thread;
+		std::condition_variable m_invoke_thread_cv;
+		std::queue<std::shared_ptr<async_action_context>> m_action_q;
+
+		std::atomic_bool m_still_running;
+		std::thread::id m_id;
 
 	};
 	
--- Include/subscription/subscription.hpp
+++ Include/subscription/subscription.hpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <mutex>
 #include <string>
+#include <map>
 #include <vector>
+#include <utils/collection.hpp>
 #include "dispatcher.hpp"
 
 
@@ -16,13 +18,11 @@ namespace afu
 	class subscription_data :
 		public std::enable_shared_from_this<subscription_data>
 	{
-
 	private:
 
 		byte_vector m_buffer;
-		
-		subscription_data() = default;
-		
+
+
 		subscription_data(size_t _data_size) :
 			m_buffer(_data_size)
 		{}
@@ -114,29 +114,113 @@ namespace afu
 	};
 
 
-
-
-	class subscriber_interface :
-		public std::enable_shared_from_this<subscriber_interface>
-
+	class rowdata_async_action:
+		public afu::async_action_context
 	{
+
+		std::function<void(const subscription_data& _data)> m_callback;
+		subscription_data m_data;
+
 	public:
+		rowdata_async_action() = default;
 
-		~subscriber_interface() = default;
 
-		virtual void subscribe(dispatcher_interface* _disp, std::function<void(const subscription_data&)>) = 0;
-		virtual void unsubscribe(dispatcher_interface* _disp) = 0;
-		
+
+		virtual void run_action() override
+		{
+			try
+			{
+				m_callback(m_data);
+			}
+			catch (const std::exception&)
+			{
+				throw std::runtime_error("callback exception\n");
+			}
+			
+		}
+
+
+
 	};
 
 
 
-	class  subscriber : public subscriber_interface
+
+
+	class  subscriber : 
+		public std::enable_shared_from_this<subscriber>
 	{
+	private:
+
+		static constexpr int POOL_BUFFER_SIZE = 10;
+
+		std::shared_ptr<afu::cyclicBuffer<subscription_data>> m_pool_buffer;
+
+		size_t m_data_size;
+
+		std::map<std::thread::id, std::pair< std::shared_ptr<afu::dispatcher>, std::function<void(const subscription_data&)>>> m_subscription_map;
+
+
+
+
 	public:
 
+		subscriber(size_t _data_size):
+			m_pool_buffer(new afu::cyclicBuffer<subscription_data>(POOL_BUFFER_SIZE)),
+			m_data_size(_data_size)
+
+		{}
+
+		virtual void subscribe(std::shared_ptr<afu::dispatcher> _disp, std::function<void(const subscription_data&)> _func) 
+		{
+			if (_disp == nullptr)
+				throw std::runtime_error("_disp == nullptr");
+
+			if (_func == nullptr)
+				throw std::runtime_error("_disp == nullptr");
+
+			auto disp_id = _disp->get_id();
+			if (m_subscription_map.find(disp_id) == m_subscription_map.end())
+			{
+				m_subscription_map[disp_id] = { _disp, _func };
+			}
+			
+		}
+		
+
+		template<typename T>
+	    void write(const T& _val)
+		{
+			if (sizeof(T) != m_data_size)
+				throw std::invalid_argumenta("sizeof(T) != m_data_size");
+
+			m_pool_buffer->push(_val);
+			//Notify()
+		}
+
+		void notify()
+		{
+
+		}
+
+		template<typename T>
+		const T& get_last()
+		{
+			if (sizeof(T) != m_data_size)
+				throw std::invalid_argumenta("sizeof(T) != m_data_size");
+			
+			return m_pool_buffer->front().read<T>();
+		}
 
 
+		template<typename T>
+		const T& get_last_i()
+		{
+			if (sizeof(T) != m_data_size)
+				throw std::invalid_argumenta("sizeof(T) != m_data_size");
+
+			return m_pool_buffer->front().read<T>();
+		}
 
 
 
